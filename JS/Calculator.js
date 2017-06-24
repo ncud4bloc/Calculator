727f8875d59ed3f30860b194f20799babceaa1fd
Neil Cudden <ncudden@sbcglobal.net>
2017-06-23 22:58:14 -0700

    Initial commit of calculator work

--- JS/Calculator.js
+++ JS/Calculator.js
@@ -48,10 +48,83 @@ var clickCount = 0;
 // Functions:
 
 var help = function(){
-    alert('This calculator uses Reverse Polish Notation, RPN, where the numbers are input first followed by the desired mathematical operand.\n\nFor example,\n\nto multiply 3 times 5 you would enter:\n\n"3 - Enter - 5 - *", to get 15.\n\nThe "back" button is a backspace to correct input typos.\nThe "dec" button sets the number of places after the decimal point.\nThe "Clear" button erases all display fields.');}
+    alert('This calculator uses Reverse Polish Notation, RPN, where the numbers are input first followed by the desired mathematical operand.\n\nFor example,\n\nto multiply 3 times 5 you would enter:\n\n"3 - Enter - 5 - *", to get 15.\n\nThe "back" button is a backspace to correct input typos.\nThe "dec" button sets the number of places after the decimal point.\nThe "Clear" button erases all display fields.');
+}
+
+var makeFloat = function(arrayInput){
+    return parseFloat(num1Str);
+}
+
+var fixNumDec = function(){
+    var decNum = prompt('Enter desired number of places after the decimal point:');
+}
 
 // Events:
 
 $howTo.on('click',function(){
     help();
+});
+
+$numPlaces.on('click',function(){
+    fixNumDec();
+});
+
+$zero.on('click',function(){
+    currentInput.push("0");
+    console.log(currentInput);
+});
+
+$one.on('click',function(){
+    currentInput.push("1");
+    console.log(currentInput);
+});
+
+$two.on('click',function(){
+    currentInput.push("2");
+    console.log(currentInput);
+});
+
+$three.on('click',function(){
+    currentInput.push("3");
+    console.log(currentInput);
+});
+
+$four.on('click',function(){
+    currentInput.push("4");
+    console.log(currentInput);
+});
+
+$five.on('click',function(){
+    currentInput.push("5");
+    console.log(currentInput);
+});
+
+$six.on('click',function(){
+    currentInput.push("6");
+    console.log(currentInput);
+});
+
+$seven.on('click',function(){
+    currentInput.push("7");
+    console.log(currentInput);
+});
+
+$eight.on('click',function(){
+    currentInput.push("8");
+    console.log(currentInput);
+});
+
+$nine.on('click',function(){
+    currentInput.push("9");
+    console.log(currentInput);
+});
+
+$point.on('click',function(){
+    currentInput.push(".");
+    console.log(currentInput);
+});
+
+$undo.on('click',function(){
+    currentInput.pop();
+    console.log(currentInput);
 });
